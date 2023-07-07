3139010fc4153a54ffbe809aa618e39e363f072a
Darya Salauyova <daryasalauyova@MacBook-Air-Darya.local>
2023-07-07 17:44:34 +0300

    Исправлена 8 задача

---
+++ task08/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+int count = 2;
+
+while (count <= n)
+{
+    Console.WriteLine(count);
+    count += 2;
+}
