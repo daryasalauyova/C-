cf5e0b162def4cb01b473bb0ff926b9a8a78ee21
Darya Salauyova <daryasalauyova@MacBook-Air-Darya.local>
2023-07-18 13:15:31 +0300

    Добавленна домашнее задание семинара 6

---
+++ task041/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.WriteLine("Введите количетво: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[M];
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine("Введите число; ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < M; i++)
+{
+    int count = 0;
+    if (array[i] > 0)
+    {
+        count+=1;
+    }
+}
+Console.WriteLine($"Колличество положительных чисел:{count}");
