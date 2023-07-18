3bafe73357c28de3375e097fe8700a2107690da1
Darya Salauyova <daryasalauyova@MacBook-Air-Darya.local>
2023-07-18 14:06:24 +0300

    Добавлены задачи семинара 8

---
+++ task053/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задайте двумерный массив. 
+Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив: 
+8 4 2 4
+5 9 2 3
+1 4 7 2
+*/
+
+int[,] Get2DArray(int rowNumber, int colNumber)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Replace(int[,] array)
+{
+    int temp = 0;
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = array [0,i];
+        array[0, i] = temp;
+    }
+}
+
+int[,] arrayRnd = GetRandom2DArray(3, 4, 10);
+Print2DArray(arrayRnd);
+Console.WriteLine();
+Replace(arrayRnd);
+Print2DArray(arrayRnd);
---
+++ task054/Program.cs
@@ -0,0 +1,88 @@
+﻿/*
+Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] GetRandom2dArray(int rows, int columns, int deviation)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] FindSumOfNumber(int[,] array)
+{
+    int[] sumInArray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumInArray[i] += array[i, j];
+        }
+    }
+    return sumInArray;
+}
+
+void PrintSumArray(int[] array)
+{
+    Console.Write("   \t");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} , ");
+
+    }
+}
+
+int  MinSumInArray(int[] array)
+{
+    int min = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= array[min])
+        {
+            min = i;
+        } 
+    }return min;
+}
+
+
+int[,] matrix = GetRandom2dArray(3, 5, 10);
+Print2DArray(matrix);
+System.Console.WriteLine();
+int[] newArray = FindSumOfNumber(matrix);
+System.Console.WriteLine();
+int result = MinSumInArray(newArray);
+System.Console.WriteLine($" Строка с наименьшей суммой чисел - {result+1}");
